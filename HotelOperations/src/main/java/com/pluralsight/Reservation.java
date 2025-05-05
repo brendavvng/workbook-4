c9261b912fce2bbdc98e7f2d0d9b579e74363152
Brenda Vang <vangsbrenda@gmail.com>
2025-05-05 16:40:36 -0500

    editing Reservation class, adding getters and setters

--- HotelOperations/src/main/java/com/pluralsight/Reservation.java
+++ HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -2,18 +2,61 @@ package com.pluralsight;
 
 public class Reservation {
 
-    private String firstName;
-    private String lastName;
+    // room types can only be king or double
+    // king room = $139 per night
+    // double room = $124 per night
+    // If the stay is over a weekend, the price per night should increase by 10%
 
-    public String getFirstName(){
-        return firstName;
-    }
-    public String getLastName(){
-        return firstName;
-    }
-    // a calculated getter
-    public String getFullName() {
-        return firstName + " " + lastName;
+    private String roomType;
+    private double price;
+    private int numberOfNights;
+    private boolean weekend;
+    private double reservationTotal;
+
+    // creating constructors
+    public Reservation(String roomType, double price, int numberOfNights, boolean weekend, double reservationTotal) {
+        this.roomType = roomType;
+        this.price = price;
+        this.numberOfNights = numberOfNights;
+        this.weekend = weekend;
+        this.reservationTotal = reservationTotal;
     }
 
+    // generating getters & setters for room type
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    // creating getter for price
+    public double getPrice() {
+        return price;
+    }
+
+    // generating getter & setters for number of nights
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    // generating getters & setters for weekend
+
+    public boolean isWeekend() {
+        return weekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        this.weekend = weekend;
+    }
+
+    // generating getter for reservation total
+    public double getReservationTotal() {
+        return reservationTotal;
+    }
 }
--- HotelOperations/src/main/java/com/pluralsight/Room.java
+++ HotelOperations/src/main/java/com/pluralsight/Room.java
@@ -4,5 +4,40 @@ public class Room {
 
     // isAvailable will be under isOccupied and isDirty
 
+    // creating private variables
+    private int numberOfBeds;
+    private double price;
+    private boolean occupied;
+    private boolean dirty;
 
+    // creating constructor
+    public Reservation(int numberOfBeds, double price, boolean occupied, boolean dirty) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.occupied = occupied;
+        this.dirty = dirty;
+    }
+
+    // generating getters
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    // creating derived getter
+    // room is only available if it is clean and vacant
+    public boolean isAvailable() {
+        return !isOccupied() && !isDirty();
+    }
 }
