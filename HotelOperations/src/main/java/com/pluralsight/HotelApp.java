68d3379a28f4c2d7e47d19f11421299e459cfec0
Brenda Vang <vangsbrenda@gmail.com>
2025-05-05 22:37:31 -0500

    Completing HotelOperations exercise

--- HotelOperations/src/main/java/com/pluralsight/HotelApp.java
+++ HotelOperations/src/main/java/com/pluralsight/HotelApp.java
@@ -5,8 +5,29 @@ public class HotelApp {
     // creating main method
     public static void main(String[] args) {
 
-        Employee emp = new Employee();
+        System.out.println("\nWelcome to Brenda's Hotel App Tester!");
+        System.out.println("~---------------------------------------~\n");
 
+        // printing out reservation info
+        Reservation reso1 = new Reservation("king", 3, true);
+        System.out.printf("Reservation #1 total: $%.2f", reso1.getReservationTotal());
+
+        System.out.println(" ");
+        Reservation reso2 = new Reservation("double", 2, false);
+        System.out.printf("Reservation #2 total: $%.2f", reso2.getReservationTotal());
+        System.out.println();
+
+
+        System.out.println("\n~---------------------------------------~");
+        System.out.println("Employee information below: \n");
+
+
+        // testing employee info
+        Employee emp = new Employee("1290", "Winnie", "Front Desk", 21, 40);
+        System.out.printf(emp.getName() + " is a demure kitty-cat employee who works at the " + emp.getDepartment() + ".");
+        System.out.println();
+        System.out.printf("%s has worked %.2f hours this week.\n", emp.getName(), emp.getHoursWorked());
+        System.out.printf(emp.getName() + ("'s Total Pay for one week: $%.2f"), emp.getTotalPayRate());
 
 
     }
--- HotelOperations/src/main/java/com/pluralsight/Reservation.java
+++ HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -7,19 +7,23 @@ public class Reservation {
     // double room = $124 per night
     // If the stay is over a weekend, the price per night should increase by 10%
 
+    // king or double
     private String roomType;
+    // price per night
     private double price;
+    // number of nights people will stay
     private int numberOfNights;
+    // if person will be staying the weekend (10% increase on weekends)
     private boolean weekend;
-    private double reservationTotal;
 
     // creating constructors
-    public Reservation(String roomType, double price, int numberOfNights, boolean weekend, double reservationTotal) {
+    public Reservation(String roomType, int numberOfNights, boolean weekend) {
         this.roomType = roomType;
-        this.price = price;
         this.numberOfNights = numberOfNights;
         this.weekend = weekend;
-        this.reservationTotal = reservationTotal;
+        // calculating price per night
+        this.price = calcPricePerNight(roomType);
+
     }
 
     // generating getters & setters for room type
@@ -29,6 +33,8 @@ public class Reservation {
 
     public void setRoomType(String roomType) {
         this.roomType = roomType;
+        // this will recalculate the price based on the room type
+        this.price = calcPricePerNight(roomType);
     }
 
     // creating getter for price
@@ -55,8 +61,27 @@ public class Reservation {
         this.weekend = weekend;
     }
 
-    // generating getter for reservation total
+    // creating method to calc price per night depending on room type
+    private double calcPricePerNight(String roomType) {
+        double pricePerNight = 0.0;
+        if("king".equalsIgnoreCase(roomType)) {
+            pricePerNight = 139.00;
+        } else if ("double".equalsIgnoreCase(roomType)) {
+            pricePerNight = 124.00;
+        } if (weekend) {
+            // for weekends, calculating the price per night * 10% increase
+            pricePerNight += pricePerNight * 0.10;
+        }
+        return pricePerNight;
+    }
+
+    // creating method to calculate the total reservation cost
     public double getReservationTotal() {
-        return reservationTotal;
+        return price * numberOfNights;
+    }
+
+    // formatting the reservation total with 2 decimal places
+    public String formatReservationTotal() {
+        return String.format("$%.2f", getReservationTotal());
     }
 }
